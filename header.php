b13091205caa5839caf0d4475cc498cdb21d992b
Thomas Strub <thomstrub@gmail.com>
2021-04-26 16:28:42 -0700

    adds the loop and customizable content

--- header.php
+++ header.php
@@ -36,6 +36,6 @@
   <div class="container">
 
     <div class="blog-header">
-      <h1 class="blog-title"><?php echo get_bloginfo( 'name' ); ?></h1>
+      <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
       <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
     </div>
