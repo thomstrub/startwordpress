4ec11afdd8c7082548ca908b8a2505686e7490b6
Thomas Strub <thomstrub@gmail.com>
2021-04-27 17:29:45 -0700

    makes sidebar dynamic

--- sidebar.php
+++ sidebar.php
@@ -2,15 +2,13 @@
   <div class="sidebar-module sidebar-module-inset">
     <h4>About</h4>
     <p>
-      Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur
-      purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
+      Web master and plant nerd. Learning to wordpress efficiently.
     </p>
   </div>
   <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">
-      <li><a href="#">March 2014</a></li>
-      <!-- More archive examples -->
+        <?php  wp_get_archives( 'type=monthly' ); ?>
     </ol>
   </div>
   <div class="sidebar-module">
